da8b9cb044ed1810e735518555eeaf61c1b26444
sash5borys <sash5borys@gmail.com>
2020-08-23 10:42:45 +0300

    dissabled vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/phone_directory'
-        : '/'
+    // publicPath: process.env.NODE_ENV === 'production'
+    //     ? '/phone_directory'
+    //     : '/'
 }
